52cccaa0f87cc43f85d4c5d3440abaef61f02cee
Douped <anthony.rooyakkers2001@gmail.com>
2023-06-09 17:07:09 -0400

    Added associations between models.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsToOne(Category);
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Products.belongsToMany(Tags,{
+  through:{
+    model: ProductTag,
+    unique: false
+  }
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tags.belongsToMany(Products,{
+  through:{
+    model: ProductTag,
+    unique: false
+  }
+});
 module.exports = {
   Product,
   Category,
