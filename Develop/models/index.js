66c960d71d25d74559d12d35f7657122072b4692
Douped <anthony.rooyakkers2001@gmail.com>
2023-06-13 00:49:51 -0400

    modifed models/index to handle foreign keys. Completed functions for tag-routes, applied code formatting to models.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,26 +1,28 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: "product_id",
+});
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag,{
-  through:{
+Product.belongsToMany(Tag, {
+  through: {
     model: ProductTag,
-    unique: false
-  }
+  },
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product,{
-  through:{
+Tag.belongsToMany(Product, {
+  through: {
     model: ProductTag,
-    unique: false
-  }
+  },
 });
 module.exports = {
   Product,
