2e5a0ef56ae8ea2e7312bf61ab39d34fe470bb67
Douped <anthony.rooyakkers2001@gmail.com>
2023-06-12 23:14:42 -0400

    added put and delete routes for categories

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,18 +5,18 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsToOne(Category);
+Product.belongsTo(Category);
 // Categories have many Products
 Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-Products.belongsToMany(Tags,{
+Product.belongsToMany(Tag,{
   through:{
     model: ProductTag,
     unique: false
   }
 });
 // Tags belongToMany Products (through ProductTag)
-Tags.belongsToMany(Products,{
+Tag.belongsToMany(Product,{
   through:{
     model: ProductTag,
     unique: false
